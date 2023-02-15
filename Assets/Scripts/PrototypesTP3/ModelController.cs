e56e3f5eb23f35db8d999103b111e58e3cde0c95
TORLeProgrammeur <henri.bernard.stl@gmail.com>
2023-02-15 15:38:40 -0500

    Update Prototype Animation

--- Assets/Scripts/PrototypesTP3/ModelController.cs
+++ Assets/Scripts/PrototypesTP3/ModelController.cs
@@ -18,9 +18,17 @@ public class ModelController : MonoBehaviour
 
     public float rotationSpeed = 50f;
 
+    public bool enableAlbedo;
+    public bool enableBumpMap;
+    public bool enableMetallicSmoothness;
+
     // Start is called before the first frame update
     void Start()
     {
+        enableAlbedo = true;
+        enableBumpMap = true;
+        enableMetallicSmoothness = true;
+
         previousButton.onClick.AddListener(() => HandlePreviousModelAction());
         nextButton.onClick.AddListener(() => HandleNextModelAction());
 
@@ -47,10 +55,8 @@ public class ModelController : MonoBehaviour
     }
     private void HandleNextModelAction()
     {
-        for (var i = gameObjectVizualizer.transform.childCount - 1; i >= 0; i--)
-        {
-            Destroy(gameObjectVizualizer.transform.GetChild(i).gameObject);
-        }
+
+        DestroyModels();
 
         index++;
         if (index > (prefabsRef.Length - 1))
@@ -58,15 +64,67 @@ public class ModelController : MonoBehaviour
             index = 0;
         }
 
+        InstanciateModel();
+    }
+
+    private void DestroyModels()
+    {
+        for (var i = gameObjectVizualizer.transform.childCount - 1; i >= 0; i--)
+        {
+            Destroy(gameObjectVizualizer.transform.GetChild(i).gameObject);
+        }
+    }
+
+    private void InstanciateModel()
+    {
         GameObject obj = prefabsRef[index];
 
         GameObject objectInstaciated = Instantiate(obj);
         objectInstaciated.transform.SetParent(gameObjectVizualizer.transform, false);
+
+        Renderer renderer = objectInstaciated.GetComponent<Renderer>();
+        Material[] materials = renderer.materials;
+
+        foreach (Material material in materials)
+        {
+            material.mainTexture = enableAlbedo ? material.mainTexture : null;
+
+            material.SetTexture("_BumpMap", enableBumpMap ? material.GetTexture("_BumpMap") : null);
+
+            // Set the Metallic and Smoothness properties of the material to 0 to disable them, or to 1 to enable them
+            material.SetFloat("_Metallic", enableMetallicSmoothness ? material.GetFloat("_Metallic") : 0f);
+            material.SetFloat("_Glossiness", enableMetallicSmoothness ? 1f : 0f);
+        }
+
+        // Assign the modified material to the game object's renderer
+        renderer.materials = materials;
+
     }
 
     void Update()
     {
         gameObjectVizualizer.transform.Rotate(Vector3.up * Time.deltaTime * rotationSpeed, Space.World);
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            enableAlbedo = !enableAlbedo;
+            DestroyModels();
+            InstanciateModel();
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            enableBumpMap = !enableBumpMap;
+            DestroyModels();
+            InstanciateModel();
+        }
+
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            enableMetallicSmoothness = !enableMetallicSmoothness;
+            DestroyModels();
+            InstanciateModel();
+        }
     }
 
 }
