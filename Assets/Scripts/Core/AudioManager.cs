fdf026ca4d177e213d2a8011e9ff80c0790de907
frank-VLS <103222132+frank-VLS@users.noreply.github.com>
2023-03-06 19:56:36 -0500

    Audio dictionnary
    Implémentation du dictionnaire de clip audio

--- Assets/Scripts/Core/AudioManager.cs
+++ Assets/Scripts/Core/AudioManager.cs
@@ -10,9 +10,68 @@ public enum AUDIO_CHANNEL
     SFX
 };
 
+public enum SOUND
+{
+    // Menus
+    MOUSEOVER,                  // Implanted: false
+    CLICK,                      // Implanted: false
+    SWEEP,                      // Implanted: false
+    // Infinite mode
+    FRUIT_FALL,                 // Implanted: false
+    FRUIT_SQUASH,               // Implanted: false
+    FRUIT_INHOLE,               // Implanted: false
+    FRUIT_MOVEMENT,             // Implanted: false
+    FRUIT_TOUCHBEE,             // Implanted: false
+    POINT_GRAB,                 // Implanted: false
+    LIFE_GRAB,                  // Implanted: false
+    SQUIRREL_PANIC,             // Implanted: false
+    SQUIRREL_CAUTION,           // Implanted: false
+    BEAR_ACTION,                // Implanted: false
+    WORM_BLINK,                 // Implanted: false
+    WORM_MOVEMENT,              // Implanted: false
+    ELEVATOR_MOVEMENT_INFINITE, // Implanted: false
+    BUNNY_SFX                   // Implanted: false
+    // Legacy mode
+};
+
+
 
 public class AudioManager : MonoBehaviour
 {
+
+    public GameObject soundplayer;
+    public SoundAudioClip[] soundAudioClipArray;
+    public Dictionary<SOUND, AudioClip> soundDictionary = new Dictionary<SOUND, AudioClip>();
+
+    [System.Serializable]
+    public class SoundAudioClip
+    {
+        public SOUND sound;
+        public AudioClip clip;
+    }
+
+    private void FillSoundDictionary()
+    {
+        foreach (SoundAudioClip soundAudioClip in soundAudioClipArray)
+        {
+            soundDictionary.Add(soundAudioClip.sound, soundAudioClip.clip);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        FillSoundDictionary();
+        uiMusic.Play();
+    }
+
+    // call exemple: AudioManager.instance.PlaySound(SOUND.SQUIRREL_PANIC);
+    public void PlaySound(SOUND sound)
+    {
+        soundplayer.GetComponent<AudioSource>().clip = soundDictionary[sound];
+        soundplayer.GetComponent<AudioSource>().Play();
+    }
+
     public static AudioManager instance;
 
     public AudioSource uiMusic;
@@ -53,15 +112,9 @@ public class AudioManager : MonoBehaviour
         infiniteMusic.Play();
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        uiMusic.Play();
-    }
-
     // Update is called once per frame
     void Update()
     {
-       
+
     }
 }
