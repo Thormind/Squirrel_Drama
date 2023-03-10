ce177ffa88d3d2b2bdaf72d699dd01c500cf07a1
frank-VLS <103222132+frank-VLS@users.noreply.github.com>
2023-03-09 20:22:19 -0500

    Fruit movement and collision audio

--- Assets/Scripts/Infinite/InfiniteFruitAudio.cs
+++ Assets/Scripts/Infinite/InfiniteFruitAudio.cs
@@ -1,9 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
+using System;
 
 public class InfiniteFruitAudio : MonoBehaviour
 {
+    public AudioSource movement;
+    public AudioSource collide;
+    private float velocity;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -12,7 +18,34 @@ public class InfiniteFruitAudio : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
+    { 
         
+        velocity = GetComponent<Rigidbody2D>().velocity.x;
+        
+        if(velocity == 0.0f)
+        {
+            movement.Stop();
+            return;
+        }
+        else
+        {
+            movement.pitch = velocity/6;
+            if(!movement.isPlaying)
+            {
+                //Debug.Log("AudioSource is not playing");
+                movement.Play();
+            }
+        }
+        
+        
+    }
+
+    private void OnCollisionEnter2D (Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Border")
+        {
+            collide.volume = Math.Abs(velocity / 90);
+            collide.Play(); 
+        }
     }
 }
