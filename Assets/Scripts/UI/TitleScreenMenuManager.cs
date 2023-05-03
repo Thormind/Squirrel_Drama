b5d476684ac435590af9eba8351b9429fae38391
TORLeProgrammeur <henri.bernard.stl@gmail.com>
2023-05-03 17:50:50 -0400

    Title Screen Music Selector removed

--- Assets/Scripts/UI/TitleScreenMenuManager.cs
+++ Assets/Scripts/UI/TitleScreenMenuManager.cs
@@ -11,18 +11,10 @@ public class TitleScreenMenuManager : MonoBehaviour
     [SerializeField] private Button anyKeyButton;
     [SerializeField] private GameObject anyKeyText;
 
-    [SerializeField] private Button musicButton;
-    [SerializeField] private TMP_Text musicText;
-
-    private InputAction previous;
-    private InputAction next;
-    private bool selectNext = true;
-
     // Start is called before the first frame update
     void Start()
     {
         anyKeyButton.onClick.AddListener(() => HandleAnyKey());
-        musicButton.onClick.AddListener(() => HandleMusicChanged());
     }
 
     private void OnEnable()
@@ -30,29 +22,9 @@ public class TitleScreenMenuManager : MonoBehaviour
         AudioManager.instance.PlayIntro();
         anyKeyText.GetComponent<flashingText>().StartFlashing();
 
-        musicText.text = AudioManager.instance.GetMusicName(ScenesManager.gameMode);
-
         GlobalUIManager.instance.specificMenu = MENU.MENU_MAIN;
         GlobalUIManager.instance.SetSelected(anyKeyButton.gameObject, BUTTON.ANY_KEY);
         GlobalUIManager.selectedButton = BUTTON.ANY_KEY;
-
-        previous = GlobalUIManager.instance.UIControls.UI.Previous;
-        next = GlobalUIManager.instance.UIControls.UI.Next;
-
-        previous.Enable();
-        next.Enable();
-
-        previous.performed += HandleMusicChangedToPrevious;
-        next.performed += HandleMusicChangedToNext;
-    }
-
-    private void OnDisable()
-    {
-        previous.Disable();
-        next.Disable();
-
-        previous.performed -= HandleMusicChangedToPrevious;
-        next.performed -= HandleMusicChangedToNext;
     }
 
 
@@ -61,27 +33,5 @@ public class TitleScreenMenuManager : MonoBehaviour
         anyKeyText.GetComponent<flashingText>().StopFlashing();
 
         GlobalUIManager.instance.SetMainMenu();
-    }
-
-    public void HandleMusicChangedToPrevious(InputAction.CallbackContext context)
-    {
-        selectNext = false;
-        musicButton.GetComponent<ButtonAnimation>().OnManualSubmit();
-        musicButton.onClick.Invoke();
-    }
-
-    public void HandleMusicChangedToNext(InputAction.CallbackContext context)
-    {
-        selectNext = true;
-        musicButton.GetComponent<ButtonAnimation>().OnManualSubmit();
-        musicButton.onClick.Invoke();
-    }
-
-    public void HandleMusicChanged()
-    {
-        string musicName = AudioManager.instance.SwitchMusic(ScenesManager.gameMode, selectNext);
-        musicText.text = musicName;
-        GlobalUIManager.instance.SetSelected(anyKeyButton.gameObject, BUTTON.ANY_KEY);
-        selectNext = true;
     }
 }
